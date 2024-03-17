195682d130d1b4b2af4ff3ea55f04955c0419dcb
yordanossole <yordanos.sole@gmail.com>
2024-03-17 11:34:08 +0300

    7 main

---
+++ palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        reverseNo = 0
+        originalNo = x
+        while x>0:
+            reverseNo = reverseNo * 10 + x%10
+            x//=10
+        
+        if originalNo == reverseNo:
+            return True
+        else:
+            return False
